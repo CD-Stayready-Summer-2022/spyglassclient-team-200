dbea3b73f614d2b12a1ee239afcc2644e8398496
Ken Cooper <kennethsecooper@gmail.com>
2022-08-05 19:53:06 -0400

    updates

--- spyglass-client/src/services/UserService.js
+++ spyglass-client/src/services/UserService.js
@@ -1,1 +1,21 @@
 import axios from "axios";
+const USER_BASE_API_URI = 'http:://localhost:8080:/api/v1/users';
+
+export function createUser(user){
+    return axios.post(USER_BASE_API_URI, user);
+}
+export function getUserById(id){
+    return axios.get(`${USER_BASE_API_URI}/${id}`);
+}
+export function getAllUsers(){
+    return axios.get(USER_BASE_API_URI);
+}
+export function updateUser(id, user){
+    return axios.put(`${USER_BASE_API_URI}/${id}`,user)
+}
+export function deleteEmployee(id){
+    return axios.delete(`${USER_BASE_API_URI}/${id}`);
+}
+export function getUserByEmail(email){
+    return axios.get(`${USER_BASE_API_URI}/lookup?email=${email}`);
+}
