e80375da02ba2b39af39b9331ef063de0dd2ce31
Boyce007 <diboyce21@students.desu.edu>
2022-08-05 11:25:39 -0400

    Home

--- spyglass-client/src/App.js
+++ spyglass-client/src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import {Home} from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route/>
+        <Route path="/home" element = {<Home/>}/>
 
       </Routes>
     </BrowserRouter>
